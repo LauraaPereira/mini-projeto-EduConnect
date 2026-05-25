0e3bb7e0ca98d447fa381dc9c865716af4c328a7
JULIA MAYUMI HAYASHI <00123374@unisoacad.uniso.br>
2026-05-25 20:32:38 -0300

    Adiciona campo de RA no formulário e na listagem. Resolve #4

--- script.js
+++ script.js
@@ -10,17 +10,19 @@ form.addEventListener('submit', (e) => {
     // 3. Pega os valores que o usuário digitou nos campos de texto
     const nome = document.getElementById('nome').value;
     const email = document.getElementById('email').value; // Adicionado na branch de melhoria
-    
+    const ra = document.getElementById('ra').value; // Adicionado na branch criada pela Julia
+
     // 4. Cria um novo item de lista (<li>) na memória do navegador
     const li = document.createElement('li');
     
     // 5. Define o texto que vai aparecer dentro desse item (Nome e E-mail)
-    li.textContent = `${nome} (${email})`;
-    
+    li.textContent = `RA: ${ra} - ${nome} (${email})`; // Linha modificada para incluir o RA na exibição
+
     // 6. Coloca esse novo item dentro da nossa lista (<ul>) no HTML
     lista.appendChild(li);
     
     // 7. Limpa os campos de texto para o usuário poder cadastrar o próximo aluno
     document.getElementById('nome').value = '';
     document.getElementById('email').value = '';
+    document.getElementById('ra').value = ''; // Linha para limpar o campo
 });
